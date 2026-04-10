c0120cf1587817ea2553f100cc259759e4a193ac
DerCritter <daboubet@gmail.com>
2026-04-10 19:37:03 +0200

    fix(services): reduce sector ii video zoom

--- components/Services.tsx
+++ components/Services.tsx
@@ -592,7 +592,7 @@ export const Services: React.FC<ServicesProps> = ({ onNavigate, isDark = false, 
               {sector2Seen && (
                 <iframe
                   src="https://player.vimeo.com/video/1179891679?autoplay=1&muted=1&playsinline=1&loop=1&autopause=0&controls=0&badge=0&portrait=0&byline=0&title=0&quality=1080p"
-                  className="absolute top-1/2 left-1/2 w-[220%] h-[220%] sm:w-[180%] sm:h-[180%] xl:w-[150%] xl:h-[150%] -translate-x-1/2 -translate-y-1/2"
+                  className="absolute top-1/2 left-1/2 w-[110%] h-[110%] -translate-x-1/2 -translate-y-1/2"
                   frameBorder="0"
                   allow="autoplay; fullscreen; picture-in-picture"
                   title="Modern New Build Reel"
